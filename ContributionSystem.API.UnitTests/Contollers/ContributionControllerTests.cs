42843384bfcb4b55b9e2fa728534369e469f84d4
tymoshytskyi <tymoshytskyi@TYMOSHYTSKYI>
2021-10-28 16:53:18 +0300

    Controller tests

--- ContributionSystem.API.UnitTests/Contollers/ContributionControllerTests.cs
+++ ContributionSystem.API.UnitTests/Contollers/ContributionControllerTests.cs
@@ -1,67 +1,101 @@
 using ContributionSystem.API.Controllers;
-using ContributionSystem.BusinessLogic.Services;
-using ContributionSystem.DataAccess.Contexts;
-using ContributionSystem.DataAccess.Repositories;
-using ContributionSystem.Entities.Entities;
-using ContributionSystem.ViewModels.Enums;
+using ContributionSystem.BusinessLogic.Interfaces;
 using ContributionSystem.ViewModels.Models.Contribution;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Moq;
 using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
 
 namespace ContributionSystem.API.UnitTests.Contollers
 {
     public class ContributionControllerTests
     {
-        //private readonly ContributionController contributionController;
-        //private const int CorrectStartValue = 1;
-        //private const int CorrectTerm = 3;
-        //private const int CorrectPercent = 100;
+        private readonly ContributionController contributionController;
 
-        //public ContributionControllerTests()
-        //{
-        //    contributionController = new ContributionController(new ContributionService(new ContributionRepository(new TestDbContext())));
-        //    var options = new DbContextOptionsBuilder<ContributionDbContext>().UseInMemoryDatabase(databaseName: "Products Test").Options;
-        //}
+        private const int PozitiveNumber = 1;
+        private const int Zero = 0;
 
-        //[Test]
-        //public async void Calculate_ValidRequest_OkObjectResultWithResponseCalculateContributionViewModel()
-        //{
-        //    var response = await contributionController.Calculate(GetCalculationRequest(CalculationMethodEnumView.Simple, CorrectStartValue, CorrectTerm, CorrectPercent));
-        //    var okObjectResult = response as OkObjectResult;
-        //    okObjectResult.Should().NotBeNull();
-        //    okObjectResult.Value.Should().BeOfType<ResponseCalculateContributionViewModel>();
-        //}
+        public ContributionControllerTests()
+        {
+            var mock = new Mock<IContributionService>();
+            mock.Setup(repo => repo
+                .Calculate(It.IsAny<RequestCalculateContributionViewModel>()))
+                .ReturnsAsync( new ResponseCalculateContributionViewModel());
+            mock.Setup(repo => repo
+                .Calculate(null))
+                .ThrowsAsync(new Exception());
+            mock.Setup(repo => repo
+                .GetHistory(It.IsAny<RequestGetHistoryContributionViewModel>()))
+                .ReturnsAsync(new ResponseGetHistoryContributionViewModel());
+            mock.Setup(repo => repo
+                .GetHistory(null))
+                .ThrowsAsync(new Exception());
+            mock.Setup(repo => repo
+                .GetDetailsById(It.Is<int>(p => p > 0)))
+                .ReturnsAsync(new ResponseGetDetailsByIdContributionViewModel());
+            mock.Setup(repo => repo
+                .GetDetailsById(It.Is<int>(p => p <= 0)))
+                .ThrowsAsync(new Exception());
+            contributionController = new ContributionController(mock.Object);
+        }
 
-        //[Test]
-        //public async void Calculate_NullRequest_ThrowException()
-        //{
-        //    var response = await contributionController.Calculate(null);
-        //    var BadRequestObjectResult = response as BadRequestObjectResult;
-        //    BadRequestObjectResult.Should().NotBeNull();
-        //    BadRequestObjectResult.StatusCode.ToString().Should().BeEquivalentTo("400");
+        [Test]
+        public async Task GetDetailsById_Positivenumber_OkObjectResultWithResponseCalculateContributionViewModel()
+        {
+            var response = await contributionController.GetDetailsById(PozitiveNumber);
+            var okObjectResult = response as OkObjectResult;
+            okObjectResult.Should().NotBeNull();
+            okObjectResult.Value.Should().BeOfType<ResponseGetDetailsByIdContributionViewModel>();
+        }
 
-        //}
+        [Test]
+        public async Task GetDetailsById_Zero_ThrowException()
+        {
+            var response = await contributionController.GetDetailsById(Zero);
+            var BadRequestObjectResult = response as BadRequestObjectResult;
+            BadRequestObjectResult.Should().NotBeNull();
+            BadRequestObjectResult.StatusCode.ToString().Should().BeEquivalentTo("400");
 
-        //private RequestCalculateContributionViewModel GetCalculationRequest(CalculationMethodEnumView calculationMethod, decimal startValue, int term, decimal percent)
-        //{
-        //    var request = new RequestCalculateContributionViewModel
-        //    {
-        //        CalculationMethod = calculationMethod,
-        //        StartValue = startValue,
-        //        Term = term,
-        //        Percent = percent
-        //    };
+        }
 
-        //    return request;
-        //}
-    }
+        [Test]
+        public async Task GetHistory_ValidRequest_OkObjectResultWithResponseCalculateContributionViewModel()
+        {
+            var response = await contributionController.GetHistory(new RequestGetHistoryContributionViewModel());
+            var okObjectResult = response as OkObjectResult;
+            okObjectResult.Should().NotBeNull();
+            okObjectResult.Value.Should().BeOfType<ResponseGetHistoryContributionViewModel>();
+        }
 
-    public class TestDbContext : DbContext
-    {
-        public DbSet<Contribution> Contribution { get; set; }
+        [Test]
+        public async Task GetHistory_NullRequest_ThrowException()
+        {
+            var response = await contributionController.GetHistory(null);
+            var BadRequestObjectResult = response as BadRequestObjectResult;
+            BadRequestObjectResult.Should().NotBeNull();
+            BadRequestObjectResult.StatusCode.ToString().Should().BeEquivalentTo("400");
 
-        public DbSet<MonthInfo> MonthInfo { get; set; }
+        }
+
+        [Test]
+        public async Task Calculate_ValidRequest_OkObjectResultWithResponseCalculateContributionViewModel()
+        {
+            var response = await contributionController.Calculate(new RequestCalculateContributionViewModel());
+            var okObjectResult = response as OkObjectResult;
+            okObjectResult.Should().NotBeNull();
+            okObjectResult.Value.Should().BeOfType<ResponseCalculateContributionViewModel>();
+        }
+
+        [Test]
+        public async Task Calculate_NullRequest_ThrowException()
+        {
+            var response = await contributionController.Calculate(null);
+            var BadRequestObjectResult = response as BadRequestObjectResult;
+            BadRequestObjectResult.Should().NotBeNull();
+            BadRequestObjectResult.StatusCode.ToString().Should().BeEquivalentTo("400");
+
+        }
     }
 }
