4c933636ec4655210f08b462127d52bac8db1ea8
tymoshytskyi <tymoshytskyi@TYMOSHYTSKYI>
2021-11-18 19:03:45 +0200

    -

---
+++ ContributionSystem.UI.UnitTests/Components/NavbarTests.cs
@@ -0,0 +1,38 @@
+﻿using Bunit;
+using ContributionSystem.UI.Components;
+using ContributionSystem.UI.UnitTests.Common;
+using FluentAssertions;
+using Xunit;
+
+namespace ContributionSystem.UI.UnitTests.Components
+{
+    public class NavbarTests : PageTestsBaseComponent
+    {
+        [Fact]
+        public void WhenHistoryButtonClicked_NoParametersPassed_Redirect()
+        {
+            var page = TestContext.RenderComponent<Navbar>();
+            page.Find("#History").Click();
+
+            Assert.Equal("http://localhost/History", NavigationManager.Uri);
+        }
+
+        [Fact]
+        public void WhenCalculationsButtonClicked_NoParametersPassed_Redirect()
+        {
+            var page = TestContext.RenderComponent<Navbar>();
+            page.Find("#Calculations").Click();
+
+            Assert.Equal("http://localhost/Main", NavigationManager.Uri);
+        }
+
+        [Fact]
+        public void WhenPageRendered_NoParametersPassed_ExpectedMarkupRendered()
+        {
+            var page = TestContext.RenderComponent<Navbar>();
+
+            page.Find("#Calculations").Should().NotBeNull();
+            page.FindAll("#History").Should().NotBeNull();
+        }
+    }
+}
