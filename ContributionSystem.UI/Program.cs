053992360ed8612466ef9ae30c582a0422a02767
tymoshytskyi <tymoshytskyi@TYMOSHYTSKYI>
2021-12-01 13:57:21 +0200

    -

--- ContributionSystem.UI/Program.cs
+++ ContributionSystem.UI/Program.cs
@@ -29,6 +29,7 @@ namespace ContributionSystem.UI
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
                 options.ProviderOptions.DefaultAccessTokenScopes.Add("api://ac090f55-725f-4ce0-be8b-d2e5d6b300bf/TestScope");
                 options.ProviderOptions.LoginMode = "redirect";
+                options.UserOptions.RoleClaim = "roles";
             });
 
             await builder.Build().RunAsync();
