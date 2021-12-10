917ecd80a33a42314ce143377f026854f479d52c
tymoshytskyi <tymoshytskyi@TYMOSHYTSKYI>
2021-12-10 17:00:42 +0200

    -

--- ContributionSystem.BusinesLogic/Services/UserService.cs
+++ ContributionSystem.BusinesLogic/Services/UserService.cs
@@ -8,9 +8,6 @@ using System.Threading.Tasks;
 
 namespace ContributionSystem.BusinessLogic.Services
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public class UserService : IUserService
     {
         private readonly GraphServiceClient _graphClient;
